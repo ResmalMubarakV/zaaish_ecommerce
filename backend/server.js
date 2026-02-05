0f072ac1d7f7df024d2adf568ce0bae5135e32e5
Resmal <resmal@Resmals-MacBook-Pro.local>
2026-02-05 17:35:03 +0530

    added backend with auth and mongodb

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect To MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Welcome To Zaaish API")
+});
+
+// API Routes
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Sever is running on http://localhost:${PORT}`);
+})
